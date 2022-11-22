cc512b68742671dce911ac8b99e8c78c1aacc968
Jounicode <96347521+Jounicrypto@users.noreply.github.com>
2022-11-22 19:05:44 +0100

    m

---
+++ main.js
@@ -0,0 +1,60 @@
+/*
+  What Is JSON ?
+  - JavaScript Object Notation
+  - Format For Sharing Data Between Server And Client
+  - JSON Derived From JavaScript
+  - Alternative To XML
+  - File Extension Is .json
+
+  Why JSON ?
+  - Easy To Use And Read
+  - Used By Most Programming Languages And Its Frameworks
+  - You Can Convert JSON Object To JS Object And Vice Versa
+
+  JSON vs XML
+  ===================================================
+  = Text Based Format      = Markup Language        =
+  = Lightweight            = Heavier                =
+  = Does Not Use Tags      = Using Tags             =
+  = Shorter                = Not Short              =
+  = Can Use Arrays         = Cannot Use Arrays      =
+  = Not Support Comments   = Support Comments       =
+  ===================================================
+*/
+
+/*
+  JSON Syntax
+  - Data Added Inside Curly Braces {  }
+  - Data Added With Key : Value
+  - Key Should Be String Wrapped In Double Quotes
+  - Data Separated By Comma
+  - Square Brackets [] For Arrays
+  - Curly Braces {} For Objects
+
+  Available Data Types
+  - String
+  - Number
+  - Object
+  - Array
+  - Boolean Values
+  - null
+*/
+
+{
+    "string": "Adnan",
+    "number": 100,
+    "object": { "NL": "ZH",
+                "USA": "NY"
+            },
+            "array": ["HTML", "CSS", "JS"],
+            "boolean": true,
+            "null": null
+}
+
+
+/*
+  JSON
+  - API Overview
+  - Tools To Test API
+  - Preview Github API
+*/
